83d5502a1f32f925db1035a76fffe60223dd49b0
WyattChase <esnaruto@yahoo.com>
2025-05-14 23:01:43 +0000

    updates media grid

--- src/js/components/MediaGrid.jsx
+++ src/js/components/MediaGrid.jsx
@@ -1,11 +1,25 @@
+// Import all images statically at build time
+const imageModules = import.meta.glob(
+  "../../assests/MediaGrid/*.{jpg,jpeg,png,gif}",
+  {
+    eager: true,
+    import: "default",
+  }
+);
+
+const images = Object.values(imageModules);
+
 export default function MediaGrid() {
   return (
-    <section id="games" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
-      {[...Array(6)].map((_, i) => (
+    <section
+      id="games"
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8"
+    >
+      {images.map((src, i) => (
         <div key={i} className="aspect-video bg-gray-800">
           <img
-            src={`https://i.ytimg.com/vi/TRGy4eVzM1I/maxresdefault.jpg`}
-            alt="media"
+            src={src}
+            alt={`media-${i + 1}`}
             className="w-full h-full object-cover"
           />
         </div>
